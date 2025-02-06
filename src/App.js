5cab3d2900cd3821b4df362970295aa9e3f27ba3
daniel037 <daniel037@hotmail.es>
2025-02-06 16:00:34 -0500

    git ignore

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          hola mundo 
+          
         </a>
       </header>
     </div>
